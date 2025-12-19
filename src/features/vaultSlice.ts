d1f54fe5a2747372bcc59451b3387aad940a00a4
Shivansh070707 <shivansh0301@gmail.com>
2025-12-19 16:16:17 +0000

    add vault slice redux

---
+++ src/features/vaultSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { VaultMetrics } from "../hooks/useVaultData";
+
+
+interface VaultState {
+  data: VaultMetrics | null;
+  lastUpdated: number | null;
+}
+
+const initialState: VaultState = {
+  data: null,
+  lastUpdated: null,
+};
+
+const vaultSlice = createSlice({
+  name: "vault",
+  initialState,
+  reducers: {
+    setVaultData: (state, action: PayloadAction<VaultMetrics>) => {
+      state.data = action.payload;
+      state.lastUpdated = Date.now();
+    },
+    clearVaultData: (state) => {
+      state.data = null;
+      state.lastUpdated = null;
+    },
+    updatePartialVaultData: (state, action: PayloadAction<Partial<VaultMetrics>>) => {
+      if (state.data) {
+        state.data = { ...state.data, ...action.payload };
+        state.lastUpdated = Date.now();
+      }
+    },
+  },
+});
+
+export const { setVaultData, clearVaultData, updatePartialVaultData } = vaultSlice.actions;
+export default vaultSlice.reducer;
